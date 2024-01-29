a3311b781eac05176206ac99b589c1798ae42f6d
Slonetech <kengeregordon@gmail.com>
2024-01-30 00:52:31 +0300

    commented apps.py

--- playground/apps.py
+++ playground/apps.py
@@ -1,6 +1,10 @@
+# Import the AppConfig class from the django.apps module
 from django.apps import AppConfig
 
-
+# Define a custom configuration class named PlaygroundConfig, which inherits from AppConfig
 class PlaygroundConfig(AppConfig):
+    # Set the default auto field for models to BigAutoField
     default_auto_field = 'django.db.models.BigAutoField'
+    
+    # Set the name attribute for the app to 'playground'
     name = 'playground'
